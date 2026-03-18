eb12b7ece8064fabe168cddf75bc4f124aebbb49
ruanpmsouza-cell <ruan_pm_souza@estudante.sesisenai.org.br>
2026-03-18 20:06:14 +0000

    update

---
+++ atividade python/atividade14.py
@@ -0,0 +1,31 @@
+estoque ={}
+
+print ("bem-vindo ao sistema de gestão de estoque. desenvolvido por Ruan do prado ")
+while True:
+    # inicia a ação de repetir
+    operacao = input ("deseja registrar a entrada e saída de produtos? (digite 'entrada' ou 'saída') ou 'sair'").lower()
+
+
+    if operacao not in ['entrada', 'saída', 'sair']:
+        print("operacao inválida.")
+        continue
+
+    if operacao == 'sair':
+        break 
+    # quebra a ação de repetição
+    produto = input("nome do produto: ").strip()
+    qtd = int(input("quantidade: "))
+
+
+    if operacao == 'entrada':
+        estoque[produto] = estoque.get(produto, 0) + qtd
+    elif [# ignora o resto da estrutura]  operacao == 'saída': 
+        if estoque.get(produto, 0) >= qtd:
+            estoque[produto] -= qtd 
+        else: # else refere-se a condição 
+            print("Erro:produto inexistente ou estoque insuficiente.")
+
+print("\n ---Estoque Final ---")
+for p, q in estoque.items():
+    print(f"{p}: {q}")
+    # (p) refere-se a os produtos e a letra (q) a quantidade
